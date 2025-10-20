c6c5ff14488ba41f6757dfc875d4f955a87c467b
petrinaanne <petrina.rodgers@papertrail.io>
2025-10-20 14:50:12 +0100

    initial commit

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php 
+
+function unseen_files(){
+    wp_enqueue_script('main-uni-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+
+function unseen_features(){
+    add_theme_support('title-tag');
+}
+
+add_action('wp_enqueue_scripts', 'unseen_files');
+
+add_action('after_setup_theme', 'unseen_features');
+?>
